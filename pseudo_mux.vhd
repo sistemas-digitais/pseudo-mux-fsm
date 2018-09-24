caecdb2307e9deee3864f18c610dace80b5c627d
Alair Dias Junior <alair.junior@gmail.com>
2018-09-24 17:12:47 -0300

    Add comments to the ports

--- pseudo_mux.vhd
+++ pseudo_mux.vhd
@@ -20,10 +20,10 @@ use ieee.std_logic_1164.all;
 
 entity pseudo_mux is
     port (
-        CLOCK   : in	std_logic;
-        S       : in    std_logic;
-        A,B,C,D : in    std_logic;
-        Q       : out   std_logic
+        CLOCK   : in    std_logic; -- clock input
+        S       : in    std_logic; -- control input
+        A,B,C,D : in    std_logic; -- data inputs
+        Q       : out   std_logic  -- data output
     );
 end pseudo_mux;
 
