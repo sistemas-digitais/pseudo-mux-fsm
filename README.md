5fb2ff4b3c8f9ff4448399e6df14632bdffb03ec
Alair Dias Junior <alair.junior@gmail.com>
2018-09-24 16:39:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pseudo-mux-fsm
+
+An interface definition for a Finite State Machine that mimics the behavior of a MUX 4x1 using just one selection input.
---
+++ pseudo_mux.vhd
@@ -0,0 +1,33 @@
+-- pseudo_mux - A Finite State Machine that mimics the behavior of mux
+-- Copyright (C) 2018  Digital Systems Group - UFMG
+-- 
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, see <https://www.gnu.org/licenses/>.
+--
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity pseudo_mux is
+    port (
+        CLOCK   : in	std_logic;
+        CONTROL : in    std_logic;
+        A,B,C,D : in    std_logic;
+        OUTPUT  : out   std_logic
+    );
+end pseudo_mux;
+
+architecture arch of pseudo_mux is
+begin
+    
+end arch;
