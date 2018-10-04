2c78d33fb4959a0d249f5ceb55ecd6c3a81cddbb
Alair Dias Junior <alair.junior@gmail.com>
2018-10-04 19:39:41 -0300

    Add RESET port

--- pseudo_mux.vhd
+++ pseudo_mux.vhd
@@ -20,6 +20,7 @@ use ieee.std_logic_1164.all;
 
 entity pseudo_mux is
     port (
+        RESET   : in    std_logic; -- reset input
         CLOCK   : in    std_logic; -- clock input
         S       : in    std_logic; -- control input
         A,B,C,D : in    std_logic; -- data inputs
